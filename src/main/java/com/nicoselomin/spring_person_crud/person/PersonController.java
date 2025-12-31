583d7c296f6d43ab302ea723477973255cb4a990
NicoSelomin <vodounnounicodemeselomin@gmail.com>
2025-12-31 18:55:31 -0400

    feat:create PersonRepository for JPA and PersonController and each method for adding, reading, updating and deleting

---
+++ src/main/java/com/nicoselomin/spring_person_crud/person/PersonController.java
@@ -0,0 +1,67 @@
+package com.nicoselomin.spring_person_crud.person;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/persons")
+public class PersonController {
+
+    final PersonRepository personRepository;
+
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Person>> getAllPerson(){
+        return new ResponseEntity<>(personRepository.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Person> createPerson(@RequestBody Person person){
+        Person personCreated = personRepository.save(person);
+        return new ResponseEntity<>(personCreated, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Person> getPersonById(@PathVariable Long id ){
+        Optional<Person> person = personRepository.findById(id);
+        return person.map(value -> new ResponseEntity<>(value, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Person> updatePerson(@PathVariable Long id, @RequestBody Person personDetails) {
+        Optional<Person> person = personRepository.findById(id);
+
+        if (person.isPresent()) {
+            Person existingPerson = person.get();
+            existingPerson.setName(personDetails.getName());
+            existingPerson.setCity(personDetails.getCity());
+            existingPerson.setPhoneNumber(personDetails.getPhoneNumber());
+
+            Person updatetPerson = personRepository.save(existingPerson);
+
+            return new ResponseEntity<>(updatetPerson, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePerson(@PathVariable Long id){
+        Optional<Person> person = personRepository.findById(id);
+
+        if (person.isPresent()) {
+            personRepository.delete(person.get());
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
