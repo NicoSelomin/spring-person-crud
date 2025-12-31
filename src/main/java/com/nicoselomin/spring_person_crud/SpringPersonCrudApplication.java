64b05640d1ba4d3ddb6e77a1a1313492868c5b2d
NicoSelomin <vodounnounicodemeselomin@gmail.com>
2025-12-31 17:56:35 -0400

    initial commit

---
+++ src/main/java/com/nicoselomin/spring_person_crud/SpringPersonCrudApplication.java
@@ -0,0 +1,13 @@
+package com.nicoselomin.spring_person_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPersonCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPersonCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nicoselomin/spring_person_crud/SpringPersonCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicoselomin.spring_person_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPersonCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
